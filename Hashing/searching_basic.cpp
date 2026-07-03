4d2b810649a0724e6238f2dd148d8535319f4e2e
Harsh Kumar <harshpvt09@gmail.com>
2026-07-03 22:14:06 +0530

    searching

---
+++ Hashing/searching_basic.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int Hashtable[10] = {0};
+    int arr[] = {10, 12, 16, 45, 86, 99};
+    for(int i = 0; i<6; i++){
+        int index = arr[i] % 10;
+        Hashtable[index] = arr[i]; 
+    }
+
+    int input;
+    cout<<"Enter the value you want to search (10, 12, 16, 45, 86,99)"<<endl; 
+    cin>>input;
+    cout<<Hashtable[input % 10]<<" found on index "<< input%10; // 86 and 16 on same index 
+}
